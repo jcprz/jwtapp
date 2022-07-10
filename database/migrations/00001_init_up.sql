a91973082bce097c283d65d027c683886e4079bd
Julio Perez <35434017+jcprz@users.noreply.github.com>
2022-07-10 15:30:59 -0400

    Create 00001_init_up.sql

---
+++ database/migrations/00001_init_up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS USERS (
+                       ID  SERIAL PRIMARY KEY,
+                       EMAIL VARCHAR(50),
+                       PASSWORD VARCHAR(100)
+                   );
+
