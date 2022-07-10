d91e770ad9f8c51a141da448282324354c7783e7
Julio Perez <35434017+jcprz@users.noreply.github.com>
2022-07-10 15:34:24 -0400

    Create utils.go

---
+++ utils/utils.go
@@ -0,0 +1,57 @@
+package utils
+
+import (
+	"encoding/json"
+	"jwt-app/models"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/dgrijalva/jwt-go"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func ResponseJSON(w http.ResponseWriter, status int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(data)
+
+}
+
+func RespondWithError(w http.ResponseWriter, status int, message string) {
+	var error models.Error
+	error.Message = message
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(error)
+
+}
+
+func GenerateToken(user models.User) (string, error) {
+	var err error
+	secret := os.Getenv("SECRET")
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email": user.Email,
+		"iss":   "course",
+	})
+
+	tokenStr, err := token.SignedString([]byte(secret))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return tokenStr, nil
+
+}
+
+func ComparePasswords(hashedPassword string, password []byte) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	return true
+}
